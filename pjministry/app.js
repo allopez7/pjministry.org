04b36224a6a84da160eb32eaa79aa762c4b036f2
Alberto Lopez <losdodgerz@gmail.com>
2020-10-19 15:05:50 -0700

    navbar update

--- pjministry/app.js
+++ pjministry/app.js
@@ -1,2 +1,12 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+
+// Display mobile menu
+const mobileMenu = ()=>{
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.onclick = ()=>{
+    mobileMenu()
+}
