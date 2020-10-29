a0149b83a73a4ed13963e704df5cef7b876a4849
Alberto Lopez <losdodgerz@gmail.com>
2020-10-28 23:58:50 -0700

    sliding window

--- pjministry/app.js
+++ pjministry/app.js
@@ -7,6 +7,67 @@ const mobileMenu = ()=>{
     menuLinks.classList.toggle('active')
 }
 
+// click mobile menu
 menu.onclick = ()=>{
     mobileMenu()
+}
+
+// slides
+const slides = document.querySelectorAll('.slide')
+const next = document.querySelector('#next')
+const prev = document.querySelector('#prev')
+const auto = true 
+const intervalTime = 5000
+let slideInterval
+
+const nextSlide = ()=>{
+    // get current class
+    const current = document.querySelector('.current')
+    // remove current class
+    current.classList.remove('current')
+    // check for next slide
+    if(current.nextElementSibling){
+        // add current to next sibling
+        current.nextElementSibling.classList.add('current')
+    }else{
+        // Add current to start
+        slides[0].classList.add('current')
+    }
+    setTimeout(()=>current.classList.remove('current'))
+}
+
+const prevSlide = ()=>{
+    // get current class
+    const current = document.querySelector('.current')
+    // remove current class
+    current.classList.remove('current')
+    // check for next slide
+    if(current.previousElementSibling){
+        // add current to next sibling
+        current.previousElementSibling.classList.add('current')
+    }else{
+        // Add current to last 
+        slides[slides.length-1].classList.add('current')
+    }
+    setTimeout(()=>current.classList.remove('current'))
+}
+
+// Button Events
+next.addEventListener('click', (e)=>{
+    nextSlide()
+    if(auto){
+        clearInterval(slideInterval)
+        slideInterval = setInterval(nextSlide, intervalTime)
+    }
+})
+prev.addEventListener('click', (e)=>{
+    prevSlide()
+    clearInterval(slideInterval)
+    slideInterval = setInterval(nextSlide, intervalTime)
+})
+
+// Auto Slide
+if(auto){
+    // run next slide at interval time
+    slideInterval = setInterval(nextSlide, intervalTime)
 }
